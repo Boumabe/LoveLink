78f0a2feb94e8ef89694a63f104cd617acc6b40c
Boumabe <marckensbou2@gmail.com>
2026-03-18 14:03:14 -0400

    Update App.js

--- App.js
+++ App.js
@@ -8,11 +8,12 @@ import {
 // On s'assure que react-native-web est bien géré pour les plateformes non-mobiles
 const isWeb = Platform.OS === 'web';
 
-// ══════════════════════════════
-//  CONFIG
-// ══════════════════════════════
+  // =============================
+// CONFIG
+// =============================
 const FB_URL = "https://lovelink-a8e75-default-rtdb.firebaseio.com";
 const API_URL = "/api/question";
+
 
 // ══════════════════════════════
 //  FIREBASE REST API
